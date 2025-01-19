b1c1c546898d1bf3d8412627760d436762c9a3e8
SHREYASH CHAPLE <93133616+shreyash2009@users.noreply.github.com>
2025-01-19 21:05:51 +0530

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://kanban-server-eight.vercel.app/api/v1/'
+const baseUrl = 'https://kanban-server-x8px.onrender.com/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
